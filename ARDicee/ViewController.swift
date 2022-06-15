10d878b8cb83952ab370951f6932d51c0aac1dd2
AIPERIM AKISHEVA <AIPERIM1995@GMAIL.COM>
2022-06-14 20:57:43 -0400

    cube material as a array and adding positions for Vector3

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -18,28 +18,43 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the view's delegate
         sceneView.delegate = self
-      
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+        
+        let material = SCNMaterial()
+        
+        material.diffuse.contents = UIColor.red
+        
+        cube.materials = [material]
+        
+        let node = SCNNode()
+        
+        node.position = SCNVector3(0, 0.1, -0.5)
+        
+      
+//        // Create a new scene
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//
+//        // Set the scene to the view
+//        sceneView.scene = scene
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+     
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
         
-        print("World Tracking is supported = \(ARWorldTrackingConfiguration.isSupported)")
-
         // Run the view's session
         sceneView.session.run(configuration)
+            
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
+        
         
         // Pause the view's session
         sceneView.session.pause()
