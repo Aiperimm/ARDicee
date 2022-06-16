095b8d6d951ab650e51ff9199ba5e964230c01b8
AIPERIM AKISHEVA <AIPERIM1995@GMAIL.COM>
2022-06-15 21:09:43 -0400

    touchLocation added in sceneView

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -76,6 +76,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let touchLocation = touch.location(in: sceneView)
+        }
+    }
+    
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         if anchor is ARPlaneAnchor {
            
