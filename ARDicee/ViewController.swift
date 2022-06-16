e70019a38387cdb623c84001b23c4a4534b61797
AIPERIM AKISHEVA <AIPERIM1995@GMAIL.COM>
2022-06-15 21:24:57 -0400

    made positions for dice

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -41,16 +41,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 //
         sceneView.autoenablesDefaultLighting = true
       
-//        // Create a new scene
-//        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
 //
-//        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
-//
-//        diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
-//
-//        sceneView.scene.rootNode.addChildNode(diceNode)
-//
-//        }
         
     }
     
@@ -81,7 +72,25 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             let touchLocation = touch.location(in: sceneView)
             
             let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
-            
+         
+            if let hitResult = results.first {
+               
+                //Create a new scene
+               let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+       
+               if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+       
+               diceNode.position = SCNVector3(
+                x: hitResult.worldTransform.columns.3.x,
+                y: hitResult.worldTransform.columns.3.y,
+                z: hitResult.worldTransform.columns.3.z
+               )
+       
+               sceneView.scene.rootNode.addChildNode(diceNode)
+       
+               }
+                
+            }
         }
     }
     
